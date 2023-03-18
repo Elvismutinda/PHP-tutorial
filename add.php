4aea88b8b77f6fa70795c8b701e9634e69bb1246
kalundaah <84059634+kalundaah@users.noreply.github.com>
2023-03-18 11:44:29 +0300

    phptoday

---
+++ add.php
@@ -0,0 +1,96 @@
+<?php 
+$errors = array('emal' => '','number' => '','ae' => '','edu' => '', 'schol' => '');
+$email = $educ = $schoo = '';
+$nos = 2547;
+$ag = 0;
+
+if(isset($_POST['submit'])){
+    if(empty($_POST['email'])){
+       $errors['emal'] = 'An email is required <br />';
+    }
+    else{
+        $email = $_POST['email'];
+        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+            $errors['emal'] = 'email must be a valid email adress';
+        }
+    }
+    if(empty($_POST['phonenumber'])){
+        $errors['number'] = 'A phone number is required <br />';
+    }
+    else{
+        $nos = $_POST['phonenumber'];
+        if(!filter_var($nos,FILTER_VALIDATE_INT)){
+            $errors['number'] = 'phonenumber must be valid';
+        }
+    }
+    if(empty($_POST['age'])){
+        $errors['ae'] = 'Age is required <br />';
+    }
+    else{
+        $ag = $_POST['age'];
+        if(!filter_var($ag,FILTER_VALIDATE_INT)){
+            $errors['ae'] = 'Age must only be a number';
+        }
+    }
+    if(empty($_POST['education'])){
+        $errors['edu'] = 'Education level is required <br />';
+    }
+    else{
+        $educ = $_POST['education'];
+        if(!preg_match('/^[a-zA-Z\s]+$/',$educ)){
+            $errors['edu'] = 'Level of eductation must only be letters and spaces';
+        }
+    }
+    if(empty($_POST['school'])){
+        $errors['schol'] = 'Schools learnt is required <br />';
+    }
+    else{
+        $schoo = $_POST['school'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$schoo)){
+            $errors['schol'] = 'Schools learnt must be a comma separated list';
+        }
+    }
+
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <h4>Add data 😃</h4>
+    <form action="add.php" method="POST" style="display: flex; flex-direction: column; margin: 50px 20%;">
+
+        <label for="email" style="margin:20px,0;">Your email: </label>
+        <input type="text" name="email" value ="<?php echo htmlspecialchars($email); ?>" >
+        <div class="errormessage" style="color:red; margin:20px,0;"><?php echo($errors['emal']);  ?></div>
+
+        <label for="phone number" style="margin:20px,0;">Your Phone number: </label>
+        <input type="number" name="phonenumber" value ="<?php echo htmlspecialchars($nos); ?>" >
+        <div class="errormessage" style="color:red; margin:20px,0;"><?php echo($errors['number']);  ?></div>
+
+        <label for="age" style="margin:20px,0;">Your age: </label>
+        <input type="number" name="age" value ="<?php echo htmlspecialchars($ag); ?>" >
+        <div class="errormessage" style="color:red; margin:20px,0;"><?php echo($errors['ae']);  ?></div>
+
+        <label for="level of education" style="margin:20px,0;">level of education: </label>
+        <input type="text" name="education" value ="<?php echo htmlspecialchars($educ); ?>">
+        <div class="errormessage" style="color:red; margin:20px,0;"><?php echo($errors['edu']);  ?></div>
+
+        <label for="schools studied" style="margin:20px,0;">Schools studied: </label>
+        <input type="text" name="school" value ="<?php echo htmlspecialchars($schoo); ?>">
+        <div class="errormessage" style="color:red; margin:20px,0;"><?php echo($errors['schol']);  ?></div>
+
+        <button name="submit" >submit</button>
+        
+    </form>
+</body>
+</html>
